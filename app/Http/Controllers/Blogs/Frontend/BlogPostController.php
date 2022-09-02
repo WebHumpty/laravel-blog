db85ab0416ba076a321d9720ab417c3c9f17b65c
WebHumpty <webxak31@yandex.ru>
2022-09-02 14:47:48 +0300

    добавил вывод постов в шаблон блог

--- app/Http/Controllers/Blogs/Frontend/BlogPostController.php
+++ app/Http/Controllers/Blogs/Frontend/BlogPostController.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers\Blogs\Frontend;
 
+use App\Models\Blogs\BlogPost;
+use Illuminate\View\View;
+
 class BlogPostController extends AppController
 {
+    /**
+     * вернуть страницу постов
+     */
+    public function index(): View
+    {
+        $paginator = BlogPost::with('blogCategory', 'blogAuthor')
+            ->published()
+            ->paginate(BlogPost::CLIENT_PER_PAGE);
 
+        return view('blogs.posts.index', [
+            'paginator' => $paginator,
+        ]);
+    }
 }
