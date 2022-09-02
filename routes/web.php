64c890d1deb1dd4e959ab210bad9122313fbdc2f
WebHumpty <webxak31@yandex.ru>
2022-09-02 17:24:51 +0300

    добавил в роуты маршрут автора поста блог

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Blogs\Frontend\BlogPostController;
 use App\Http\Controllers\Blogs\Frontend\BlogCategoryController;
+use App\Http\Controllers\Blogs\Frontend\BlogAuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::get('/', [BlogPostController::class, 'index'])->name('blogs.posts.index')
 Route::get('/posts/search', [BlogPostController::class, 'search'])->name('blogs.posts.search');
 Route::get('/posts/{slug}', [BlogPostController::class, 'show'])->name('blogs.posts.single');
 Route::get('/categories/{slug}', [BlogCategoryController::class, 'show'])->name('blogs.categories.single');
+Route::get('/authors/{slug}', [BlogAuthorController::class, 'show'])->name('blogs.authors.single');
