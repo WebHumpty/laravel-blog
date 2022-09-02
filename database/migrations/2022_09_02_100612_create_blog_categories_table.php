69089226dfb6cfbebb1bb2c8a420f0fa85a99f92
WebHumpty <webxak31@yandex.ru>
2022-09-02 13:09:19 +0300

    добавил модель, миграцию категории блог

---
+++ database/migrations/2022_09_02_100612_create_blog_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_categories', function (Blueprint $table) {
+            $table->increments('id')->comment('ID');
+            $table->string('slug', 100)->unique()->comment('чпу');
+            $table->string('name', 100)->unique()->comment('название');
+            $table->boolean('is_active')->default(0)->comment('статус');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_categories');
+    }
+}
