d21140122dc2f112acae2b86ed1fde7ef5353880
WebHumpty <webxak31@yandex.ru>
2022-09-02 18:54:23 +0300

    добавил базовую модель

---
+++ app/Models/Blogs/AppModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Blogs;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class AppModel extends Model
+{
+    use HasFactory;
+
+    /**
+     * вернуть дату публикации
+     */
+    public function publishedDate(): string
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $this->created_at)
+            ->format('d, F, Y');
+    }
+
+    /**
+     * вернуть посты
+     */
+    public function blogPosts(): HasMany
+    {
+        return $this->hasMany(BlogPost::class);
+    }
+}
