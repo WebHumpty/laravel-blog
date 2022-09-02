fdf2a88c6ba505be4032b8b669bf810fa59e68a8
WebHumpty <webxak31@yandex.ru>
2022-09-02 17:27:01 +0300

    добавил контр.автора, вывел посты автора в шаблон

---
+++ app/Http/Controllers/Blogs/Frontend/BlogAuthorController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Blogs\Frontend;
+
+use App\Models\Blogs\BlogAuthor;
+use App\Models\Blogs\BlogPost;
+use Illuminate\View\View;
+
+class BlogAuthorController extends AppController
+{
+    /**
+     * вернуть страницу постов автора с пагинацией
+     */
+    public function show(string $slug): View
+    {
+        $item = BlogAuthor::select('id', 'slug', 'name')
+            ->where('slug', $slug)
+            ->first();
+
+        if (!$item) {
+            abort(404);
+        }
+
+        $paginator = $item->blogPosts()
+            ->with('blogCategory', 'blogAuthor')
+            ->published()
+            ->orderBy('id', 'DESC')
+            ->paginate(BlogPost::CLIENT_PER_PAGE);
+
+        return view('blogs.authors.show', [
+            'item' => $item,
+            'paginator' => $paginator,
+        ]);
+    }
+}
