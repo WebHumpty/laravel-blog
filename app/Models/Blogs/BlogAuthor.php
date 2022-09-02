e3b0ebe2551f911cc85433fa4af93f90bf262a2a
WebHumpty <webxak31@yandex.ru>
2022-09-02 13:05:54 +0300

    добавил модель, миграцию автора блог

---
+++ app/Models/Blogs/BlogAuthor.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models\Blogs;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class BlogAuthor
+ * @package App\Models\Blogs
+ * @mixin Builder
+ */
+class BlogAuthor extends Model
+{
+    use HasFactory, Sluggable;
+
+    protected $fillable = [
+        'slug',
+        'name',
+        'email',
+    ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+}
