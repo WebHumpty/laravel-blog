a331325457dc3a3b050d784575338bd82ee61035
WebHumpty <webxak31@yandex.ru>
2022-09-05 14:09:18 +0300

    изменил получение постов для вывода в бар

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Helpers\Viewing;
 use App\Models\Blogs\BlogCategory;
 use App\Models\Blogs\BlogPost;
+use App\Repositories\Blogs\BlogPostRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,20 +29,12 @@ class AppServiceProvider extends ServiceProvider
     {
         view()->composer('blogs.blocks._sidebar', function ($view) {
            $view->with('categoriesList', BlogCategory::with('blogPosts')->get());
-           $view->with('popularPosts', BlogPost::orderBy('views', 'DESC')
-               ->limit(BlogPost::POPULAR_COUNT)
-               ->get()
-           );
 
-           $postsIds = (Viewing::create())
-               ->setKey(Viewing::RECENT_COOKIE_KEY)
-               ->getCookieIds();
+           $blogPostRepository = app(BlogPostRepository::class);
 
-           $view->with('recentPosts', BlogPost::whereIn('id', $postsIds)
-               ->orderBy('views', 'DESC')
-               ->limit(BlogPost::RECENT_COUNT)
-               ->get()
-           );
+           $view->with('popularPosts', $blogPostRepository->getPopularPosts(BlogPost::POPULAR_COUNT));
+
+           $view->with('recentPosts', $blogPostRepository->getRecentPosts(BlogPost::RECENT_COUNT));
         });
     }
 }
